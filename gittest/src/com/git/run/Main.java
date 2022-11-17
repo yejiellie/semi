2a5608bec99b5d9dbaeb3e0d76b2e6f751de64b9
GDJ56 <GDJ56@DESKTOP-DGRFDS1>
2022-11-17 09:14:38 +0900

    01_bs 정수 변수 추가, 더하기 출력

---
+++ gittest/src/com/git/run/Main.java
@@ -0,0 +1,14 @@
+package com.git.run;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Git!!");
+		
+		int a=10;
+		int b=20;
+		System.out.println(a+b);
+
+	}
+
+}
